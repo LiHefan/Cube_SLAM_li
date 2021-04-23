2c6e27df938054ec44cbb32cd1702f9ec4dfd1e7
Hefan Li <ujgue@student.kit.edu>
2021-04-23 17:09:20 +0200

    add comment

--- object_slam_li/include/object_slam_li/Object_landmark.h
+++ object_slam_li/include/object_slam_li/Object_landmark.h
@@ -3,12 +3,12 @@
 #include<vector>
 #include"g2o_Object.h"
 
-//物体路标类
-//包含立方体的信息
+//object_landmark类
+//包含cuboid的信息
 
 class object_landmark{
 public:
-    g2o::cuboid cube_meas; //立方体路标的9自由度信息
+    g2o::cuboid cube_meas; //cuboid9自由度信息
     g2o::VertexCuboid* cube_vertex; //g2o的顶点－物体测量
     double meas_quality; //评估提案测量的质量[0,1] the higher, the better
 };
